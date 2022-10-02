1ad5c125f30ed647c3dd7e84465adc42e53ec554
eito-fis <eamon@itofisher.com>
2022-10-02 13:04:13 -0400

    Initialize flake8

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,26 @@
+import nox
 from nox_poetry import session
 
+
+nox.options.sessions = "lint", "tests"
+
 python_versions = ["3.10"]
+locations = "src", "tests", "noxfile.py"
+
+
+@session(python=python_versions)
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    session.run("flake8", *args)
+
+
+@session(python=python_versions)
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
 
 @session(python=python_versions)
 def tests(session):
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,4 +1,5 @@
 import coin_test
 
+
 def test_version():
     assert coin_test.__version__ == "0.1.0"
