7812e66ea7545463bebd474b4efb91ef5ae02c36
eito-fis <eamon@itofisher.com>
2022-10-10 14:46:38 -0400

    Import in __init__.py

--- src/coin_test/data/__init__.py
+++ src/coin_test/data/__init__.py
@@ -1,1 +1,6 @@
 """Data loading / processing module."""
+
+from .dataset import Dataset
+from .processors import IdentityProcessor, Processor
+
+__all__ = ["Dataset", "IdentityProcessor", "Processor"]
