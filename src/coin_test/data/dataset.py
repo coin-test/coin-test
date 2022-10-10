ced11abe26c565fb768b50cc7367faac18cd2970
eito-fis <eamon@itofisher.com>
2022-10-10 14:22:42 -0400

    Initialize dataset class

---
+++ src/coin_test/data/dataset.py
@@ -0,0 +1,40 @@
+"""Dataset object."""
+
+import pandas as pd
+from pandas import DataFrame
+
+from .processor import Processor
+
+
+class Dataset:
+    """Object that manages a single dataset."""
+
+    def __init__(
+        self, src: str, asset: str, currency: str, processors: list[Processor]
+    ) -> None:
+        """Intialize a dataset.
+
+        Args:
+            src: CSV to load data from. Can be a URL or a file path
+            asset: Asset being traded
+            currency: Currency being traded against
+            processors: List of Processor objects to be used to transform the
+                loaded dataframe
+        """
+        self.asset = asset
+        self.currency = currency
+
+        df = self._load_df(src)
+        self.df = self._clean(df, processors)
+
+        self.interval = None
+
+    def _load_df(self, src: str) -> DataFrame:
+        """Load the dataframe from src."""
+        return pd.read_csv(src, sep=None)
+
+    def _clean(self, df: DataFrame, processors: list[Processor]) -> DataFrame:
+        """Clean the dataframe."""
+        for processor in processors:
+            df = processor.process(df)
+        return df
