db30dadcf983bc97d73bf2fe11b1ac99b61a2ad8
eito-fis <eamon@itofisher.com>
2022-10-12 23:52:01 -0400

    Add valid trading data

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,3 +22,25 @@ def simple_df() -> pd.DataFrame:
     column_names = ["Time", "Value"]
     data = [[0, 10], [1, 100], [2, 1000]]
     return pd.DataFrame(data=data, columns=column_names)
+
+
+@pytest.fixture
+def hour_data() -> str:
+    """Hourly data CSV filepath."""
+    return "tests/assets/eth_usdc_1h_9_28.csv"
+
+
+@pytest.fixture
+def hour_data_df(hour_data: str) -> pd.DataFrame:
+    """Hourly data contents."""
+    dtypes = {
+        "Open Time": int,
+        "Open": float,
+        "High": float,
+        "Low": float,
+        "Close": float,
+        "Volume": float,
+        "Close Time": int,
+    }
+    df = pd.read_csv(hour_data, dtype=dtypes)
+    return df
