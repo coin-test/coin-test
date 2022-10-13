efa4665aca8b0b6ce410bee223d7f87337c24d92
eito-fis <eamon@itofisher.com>
2022-10-12 23:54:44 -0400

    Add interval inference and dataframe validation

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -3,19 +3,51 @@
 from unittest.mock import Mock
 
 import pandas as pd
+import pytest
 from pytest_mock import MockerFixture
 
 from coin_test.data import Dataset
 
 
-def test_load_df(simple_csv: str, simple_df: pd.DataFrame) -> None:
-    """Test _load_df can load a simple CSV."""
-    df = Dataset._load_df(simple_csv)
-    pd.testing.assert_frame_equal(df, simple_df)
+def test_validate_df_correct(hour_data_df: pd.DataFrame) -> None:
+    """Validates a correctly formatted df."""
+    assert Dataset.validate_df(hour_data_df)
+
+
+def test_validate_df_missing_col(hour_data_df: pd.DataFrame) -> None:
+    """Rejects a df missing a require column."""
+    hour_data_df = hour_data_df.drop(columns=["Open"])
+    assert not Dataset.validate_df(hour_data_df)
+
+
+def test_validate_df_duplicate_col(hour_data_df: pd.DataFrame) -> None:
+    """Rejects a df missing a require column."""
+    hour_data_df.insert(0, "Open", hour_data_df["Open"], allow_duplicates=True)
+    assert not Dataset.validate_df(hour_data_df)
+
+
+def test_validate_df_incorrect_type(hour_data_df: pd.DataFrame) -> None:
+    """Rejects a df with an incorrect type."""
+    hour_data_df["Open"] = hour_data_df["Open"].astype(int)
+    assert not Dataset.validate_df(hour_data_df)
+
+
+def test_infer_interval() -> None:
+    """Inferrs the correct interval."""
+    timestamps = pd.Series([10, 20, 30], dtype=int)
+    interval = Dataset.infer_interval(timestamps)
+    assert interval == 10
+
+
+def test_infer_interval_error() -> None:
+    """Errors on inconsistent timestamps."""
+    timestamps = pd.Series([10, 20, 40], dtype=int)
+    with pytest.raises(ValueError):
+        Dataset.infer_interval(timestamps)
 
 
 def test_clean(simple_df: pd.DataFrame) -> None:
-    """Test _clean runs the processor."""
+    """Runs the processor."""
     processor = Mock()
     processor.process.return_value = simple_df
     df = Dataset._clean(simple_df, [processor])
@@ -24,23 +56,62 @@ def test_clean(simple_df: pd.DataFrame) -> None:
     processor.process.assert_called_once_with(simple_df)
 
 
-def test_init_dataset(
-    simple_csv: str, simple_df: pd.DataFrame, mocker: MockerFixture
+def test_init_dataset(simple_df: pd.DataFrame, mocker: MockerFixture) -> None:
+    """Initializes correctly."""
+    asset = "BTC"
+    currency = "USD"
+    interval = 10000
+    processors = []
+
+    mocker.patch("coin_test.data.Dataset.validate_df")
+    mocker.patch("coin_test.data.Dataset.infer_interval")
+    mocker.patch("coin_test.data.Dataset._clean")
+
+    Dataset.validate_df.return_value = True
+    Dataset._clean.return_value = simple_df
+
+    dataset = Dataset(asset, currency, processors, simple_df, interval)
+
+    assert dataset.asset == asset
+    assert dataset.currency == currency
+    assert dataset.interval == interval
+
+    Dataset.validate_df.assert_called_once_with(simple_df)
+    Dataset.infer_interval.assert_not_called()
+    Dataset._clean.assert_called_once_with(simple_df, processors)
+
+
+def test_init_dataset_infers(hour_data_df: pd.DataFrame, mocker: MockerFixture) -> None:
+    """Infers interval when not provided."""
+    asset = "BTC"
+    currency = "USD"
+    interval = 10
+    processors = []
+
+    mocker.patch("coin_test.data.Dataset.validate_df")
+    mocker.patch("coin_test.data.Dataset.infer_interval")
+    mocker.patch("coin_test.data.Dataset._clean")
+
+    Dataset.validate_df.return_value = True
+    Dataset.infer_interval.return_value = interval
+    Dataset._clean.return_value = hour_data_df
+
+    dataset = Dataset(asset, currency, processors, hour_data_df)
+
+    assert dataset.interval == interval
+    Dataset.infer_interval.assert_called_once_with(hour_data_df["Open Time"])
+
+
+def test_init_dataset_invalid_df(
+    simple_df: pd.DataFrame, mocker: MockerFixture
 ) -> None:
-    """Test initialization succeeds."""
+    """Errors on invalid df."""
     asset = "BTC"
     currency = "USD"
     processors = []
 
-    mocker.patch("coin_test.data.Dataset._load_df")
-    mocker.patch("coin_test.data.Dataset._clean")
-    Dataset._load_df.return_value = simple_df
+    mocker.patch("coin_test.data.Dataset.validate_df")
+    Dataset.validate_df.return_value = False
 
-    dataset = Dataset(simple_csv, asset, currency, processors)
-
-    assert dataset.asset == asset
-    assert dataset.currency == currency
-    assert dataset.interval is None  # Currently un-implemented
-
-    dataset._load_df.assert_called_once_with(simple_csv)
-    dataset._clean.assert_called_once_with(simple_df, processors)
+    with pytest.raises(ValueError):
+        Dataset(asset, currency, processors, simple_df)
