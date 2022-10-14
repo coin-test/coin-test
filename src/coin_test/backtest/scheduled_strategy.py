3f6a17168ea148cdee3a377c8d42442ec23b1f0a
Andrew Mascillaro <amascillaro@gmail.com>
2022-10-14 13:50:51 -0400

    Add scheduled strategy class

---
+++ src/coin_test/backtest/scheduled_strategy.py
@@ -0,0 +1,29 @@
+"""Define the ScheduledStrategy class."""
+
+import datetime as dt
+from typing import Callable
+
+from .portfolio import Portfolio
+from .trade_request import TradeRequest
+from ..data import Dataset
+
+
+class ScheduledStrategy:
+    """A class to store a trading strategy run as a scheduled job."""
+
+    def __init__(
+        self,
+        strategy: Callable[[dt.datetime, Portfolio, Dataset], list[TradeRequest]],
+        schedule: str,
+        lookback: None = None,
+    ) -> None:
+        """Initialize a ScheduledStrategy object.
+
+        Args:
+            strategy: The function called to run the strategy
+            schedule: A cron string specifying when the strategy should be run
+            lookback: The amount of data to send to the strategy function
+        """
+        self.strategy = strategy
+        self.schedule = schedule
+        self.lookback = lookback
