22dbe09a19314a6817bb798fd8f316827c230a9b
eito-fis <eamon@itofisher.com>
2022-10-18 23:11:45 -0400

    Add MetaData class definition

--- src/coin_test/data/__init__.py
+++ src/coin_test/data/__init__.py
@@ -2,6 +2,14 @@
 
 from .dataset import Dataset
 from .loaders import DataFrameLoader, Loader
+from .metadata import MetaData
 from .processors import IdentityProcessor, Processor
 
-__all__ = ["Dataset", "IdentityProcessor", "Processor", "Loader", "DataFrameLoader"]
+__all__ = [
+    "Dataset",
+    "IdentityProcessor",
+    "Processor",
+    "Loader",
+    "DataFrameLoader",
+    "MetaData",
+]
---
+++ src/coin_test/data/metadata.py
@@ -0,0 +1,11 @@
+"""Define the MetaData class."""
+
+from typing import NamedTuple
+
+
+class MetaData(NamedTuple):
+    """Historical data metadata."""
+
+    asset: str
+    currency: str
+    interval: int
