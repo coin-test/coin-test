e2eb235722a346b6ac1571b69037f6fd548ce66e
Andrew Mascillaro <amascillaro@gmail.com>
2022-10-26 14:10:20 -0400

    Remove incomplete Strategy and Simulator classes.

--- src/coin_test/backtest/__init__.py
+++ src/coin_test/backtest/__init__.py
@@ -1,9 +1,7 @@
 """Backtesting module of the coin-test library."""
 
 from .portfolio import Portfolio
-from .simulator import Simulator
-from .strategy import Strategy
 from .trade import Trade
 from .trade_request import TradeRequest
 
-__all__ = ["Portfolio", "Simulator", "Strategy", "Trade", "TradeRequest"]
+__all__ = ["Portfolio", "Trade", "TradeRequest"]
