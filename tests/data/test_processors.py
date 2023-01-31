aad76f132a898d24a14e07cd769dd5b933860023
eito-fis <eamon@itofisher.com>
2023-01-31 18:02:36 -0500

    Add fill processor

--- tests/data/test_processors.py
+++ tests/data/test_processors.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-from coin_test.data import IdentityProcessor
+from coin_test.data import FillProcessor, IdentityProcessor
 
 
 def test_identity_processor(simple_df: pd.DataFrame) -> None:
@@ -10,3 +10,13 @@ def test_identity_processor(simple_df: pd.DataFrame) -> None:
     processor = IdentityProcessor()
     new_df = processor(simple_df.copy())
     pd.testing.assert_frame_equal(simple_df, new_df)
+
+
+def test_fill_processor(hour_data_indexed_df: pd.DataFrame) -> None:
+    """Fills missing periods."""
+    processor = FillProcessor("H")
+    hour_data_indexed_df.drop(hour_data_indexed_df.index[3], inplace=True)
+
+    old_len = len(hour_data_indexed_df)
+    new_df = processor(hour_data_indexed_df)
+    assert len(new_df) == old_len + 1
