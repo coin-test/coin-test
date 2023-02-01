744bc907e28f512742490f972a738352d5b799fd
Eamon Ito-Fisher <eamon@itofisher.com>
2023-02-01 14:26:40 -0500

    Handle missing data (#43)
    * Add new static method that includes all composer param validation
    
    * Add test for missing data in composer
    
    * Add missing data validation, update tests
    
    * Finish missing data tests
    
    * Add fill processor

--- src/coin_test/data/processors.py
+++ src/coin_test/data/processors.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 
+import pandas as pd
 from pandas import DataFrame
 
 
@@ -17,5 +18,31 @@ class IdentityProcessor(Processor):
     """Identity Transform."""
 
     def __call__(self, df: DataFrame) -> DataFrame:
-        """Identity tranfsorm."""
+        """Identity transform."""
         return df
+
+
+class FillProcessor(Processor):
+    """Fill missing periods for a dataset."""
+
+    def __init__(self, freq: str, method: str | None = "pad") -> None:
+        """Initialize a FillProcessor.
+
+        Args:
+            freq: Frequency to fill periods for.
+            method: Fill method to use in pandas .fillna() function. Must be one
+                of {‘backfill’, ‘bfill’, ‘pad’, ‘ffill’, None}.
+        """
+        self.freq = freq
+        self.method = method
+
+    def __call__(self, df: DataFrame) -> DataFrame:
+        """Identity tranfsorm."""
+        full_index = pd.period_range(
+            df.index[0], df.index[-1], freq=self.freq  # type: ignore
+        )
+        missing_index = full_index.difference(df.index)
+        missing_df = pd.DataFrame(index=missing_index, columns=df.columns)
+        full_df = pd.concat([df, missing_df]).sort_index()
+        full_df = full_df.fillna(method=self.method)  # type: ignore
+        return full_df
