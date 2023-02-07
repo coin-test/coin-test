5b00dbfa5234826b95584641928f942df372ff07
teadetime <22845858+teadetime@users.noreply.github.com>
2023-02-07 12:18:41 -0500

    Sphinx Action to Deploy on Github Pages (#56)
    * Github Action to Build Sphinx docs
    
    * Set Path correctly

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+-- Project information -----------------------------------------------------
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
+autodoc_mock_imports = ["numpy", "pandas", "croniter"]
+
+project = "coin-test"
+copyright = "2022, Olin SCOPE"
+author = "Olin SCOPE"
+release = "0.1"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "python"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
