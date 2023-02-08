1006a128dcf4bc3d7fd5f2753c9436cdb753a94e
eito-fis <eamon@itofisher.com>
2023-02-08 12:39:00 -0500

    Replace start and end time with length in composer

--- src/coin_test/data/composer.py
+++ src/coin_test/data/composer.py
@@ -16,18 +16,16 @@ class Composer:
     def __init__(
         self,
         datasets: list[PriceDataset],
-        start_time: pd.Timestamp,
-        end_time: pd.Timestamp,
+        length: pd.Timedelta | pd.DateOffset,
     ) -> None:
         """Intialize a dataset.
 
         Args:
             datasets: List of PriceDatasets containing all price information for
                 use in a simulation run.
-            start_time: Expected start time to validate datasets against
-            end_time: Expected end time to validate datasets against
+            length: Length of time for simulation run.
         """
-        shared_currency = self._validate_params(datasets, start_time, end_time)
+        start_time, end_time, shared_currency = self._validate_params(datasets, length)
         self.start_time = start_time
         self.end_time = end_time
         self.currency = shared_currency
@@ -37,52 +35,46 @@ class Composer:
     @staticmethod
     def _validate_params(
         datasets: list[PriceDataset],
-        start_time: pd.Timestamp,
-        end_time: pd.Timestamp,
-    ) -> Ticker:
+        length: pd.Timedelta | pd.DateOffset,
+    ) -> tuple[pd.Timestamp, pd.Timestamp, Ticker]:
         """Validates input parameters.
 
         Args:
             datasets: List of PriceDatasets containing all price information for
                 use in a simulation run.
-            start_time: Expected start time to validate datasets against
-            end_time: Expected end time to validate datasets against
+            length: Length of time for simulation run.
 
         Returns:
+            pd.Timestamp: Validated start time of simulation
+            pd.Timestamp: Validted end time of simulation
             Ticker: Currency shared by all datasets
 
         Raises:
-            ValueError: If start and end time are invalid, if datasets do not
-                align to start and end times or if datasets do not share a base
-                currency.
+            ValueError: If datasets are not specified, if length does not fit
+                datasets overlap, or if datasets don't share a currency.
         """
-        Composer._validate_start_end(start_time, end_time)
-
         if len(datasets) == 0:
             raise ValueError("At least one dataset must be defined.")
 
         for ds in datasets:
-            if not Composer._is_within_range(ds, start_time, end_time):
-                raise ValueError(f"Dataset {ds} does not cover time range.")
             if not Composer._validate_missing_data(ds):
                 raise ValueError(f"Dataset {ds} does not have data for every period.")
+
+        start_end = Composer._get_start_end(datasets, length)
+        if start_end is None:
+            raise ValueError("Overlapping time for datasets less than requested length")
+        start_time, end_time = start_end
 
         shared_currency = Composer._get_shared_currency(datasets)
         if shared_currency is None:
             raise ValueError("Not all datasets share a single currency.")
-        return shared_currency
+
+        return start_time, end_time, shared_currency
 
     @staticmethod
     def _validate_start_end(start: pd.Timestamp, end: pd.Timestamp) -> None:
         if start > end:
             raise ValueError("Start time must be earlier than end time.")
-
-    @staticmethod
-    def _is_within_range(
-        dataset: PriceDataset, start_time: pd.Timestamp, end_time: pd.Timestamp
-    ) -> bool:
-        """Check whether dataset has data for start and end time."""
-        return not dataset.df[:start_time].empty and not dataset.df[end_time:].empty
 
     @staticmethod
     def _validate_missing_data(dataset: PriceDataset) -> bool:
@@ -92,6 +84,25 @@ class Composer:
         )
         missing_index = full_index.difference(dataset.df.index)
         return len(missing_index) == 0
+
+    @staticmethod
+    def _get_start_end(
+        datasets: list[PriceDataset], length: pd.Timedelta | pd.DateOffset
+    ) -> tuple[pd.Timestamp, pd.Timestamp] | None:
+        """Get start and time for datasets."""
+        start_times = [
+            dataset.df.index[0].start_time for dataset in datasets  # type: ignore
+        ]
+        end_times = [
+            dataset.df.index[-1].end_time for dataset in datasets  # type: ignore
+        ]
+
+        start_time = max(start_times)
+        end_time = min(end_times)
+        if start_time + length > end_time:
+            return None
+
+        return start_time, start_time + length
 
     @staticmethod
     def _get_shared_currency(datasets: list[PriceDataset]) -> Ticker | None:
