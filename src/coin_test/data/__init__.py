010017f2d1bbe56739c5c083f244080f249f2ca7
Nathan Faber <ntfaber@gmail.com>
2023-02-13 13:42:34 -0500

    datasaver tests

--- src/coin_test/data/__init__.py
+++ src/coin_test/data/__init__.py
@@ -1,6 +1,7 @@
 """Data loading / processing module."""
 
 from .composer import Composer
+from .datasaver import Datasaver
 from .datasets import CustomDataset, Dataset, PriceDataset
 from .metadata import MetaData
 from .processors import FillProcessor, IdentityProcessor, Processor
@@ -8,6 +9,7 @@ from .processors import FillProcessor, IdentityProcessor, Processor
 __all__ = [
     "Composer",
     "CustomDataset",
+    "Datasaver",
     "Dataset",
     "PriceDataset",
     "MetaData",
---
+++ tests/data/test_datasaver.py
@@ -0,0 +1,92 @@
+"""Test the DataSaver class."""
+
+import os
+import pickle
+from unittest.mock import Mock, mock_open, patch
+
+from pytest_mock import MockerFixture
+
+from coin_test.data import Datasaver, Dataset
+
+
+def test_datasaver_valid_construction_list() -> None:
+    """Construct Datasaver using a single dpeth list."""
+    name = "test_datasaver"
+    datasets = [Mock(spec=Dataset)]
+    dsaver = Datasaver(name, datasets)  # pyright: ignore
+
+    assert name == dsaver.name
+    assert [datasets] == dsaver.datasets_lists
+
+
+def test_datasaver_valid_construction_list_list() -> None:
+    """Construct Datasaver using a single dpeth list."""
+    name = "test_datasaver12"
+    datasets = [[Mock(spec=Dataset), Mock(spec=Dataset)], [Mock(spec=Dataset)]]
+    dsaver = Datasaver(name, datasets)  # pyright: ignore
+
+    assert name == dsaver.name
+    assert datasets == dsaver.datasets_lists
+
+
+def test_datasaver_save(mocker: MockerFixture) -> None:
+    """Pickle Datasaver object properly."""
+    dsaver = Mock(spec=Datasaver)
+    dsaver.name = "test"
+    valid_local_path = "a_valid_path"
+
+    mocker.patch("pickle.dump")
+    pickle.dump.return_value = None
+
+    mocker.patch("os.path.exists")
+    os.path.exists.return_value = True
+
+    with patch("builtins.open", mock_open(read_data="data")):  # as mock_file:
+        final_path = Datasaver.save(dsaver, valid_local_path)
+        pickle.dump.assert_called()
+
+        # TODO: One Mockfile has a spec?
+        # pickle.dump.assert_called_with(dsaver, mock_file, pickle.HIGHEST_PROTOCOL)
+
+    assert final_path == valid_local_path + "/" + dsaver.name + ".pkl"
+
+
+def test_datasaver_save_new_dir(mocker: MockerFixture) -> None:
+    """Pickle Datasaver object properly."""
+    dsaver = Mock(spec=Datasaver)
+    dsaver.name = "test"
+    valid_local_path = "a_valid_path"
+
+    mocker.patch("pickle.dump")
+    pickle.dump.return_value = None
+
+    mocker.patch("os.path.exists")
+    os.path.exists.return_value = False
+
+    mocker.patch("os.makedirs")
+
+    with patch("builtins.open", mock_open(read_data="data")):  # as mock_file:
+        final_path = Datasaver.save(dsaver, valid_local_path)
+        pickle.dump.assert_called()
+
+        # TODO: One Mockfile has a spec?
+        # pickle.dump.assert_called_with(dsaver, mock_file, pickle.HIGHEST_PROTOCOL)
+
+    assert final_path == valid_local_path + "/" + dsaver.name + ".pkl"
+
+
+def test_datasaver_load(mocker: MockerFixture) -> None:
+    """Load Pickled Datasaver object properly."""
+    datasaver = Mock()
+    valid_local_path = "a_valid_path/test.pkl"
+
+    mocker.patch("pickle.load")
+    pickle.load.return_value = datasaver
+
+    with patch("builtins.open", mock_open(read_data="data")):  # as mock_file:
+        test = Datasaver.load(valid_local_path)
+        pickle.load.assert_called()
+        assert test == datasaver
+
+        # TODO: One Mockfile has a spec?
+        # pickle.load.assert_called_with(mock_file)
