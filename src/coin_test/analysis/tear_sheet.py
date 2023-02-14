43be318735a5bb1a8ec96c8d03a4950bbb1795a7
Andrew Mascillaro <amascillaro@gmail.com>
2023-02-14 15:47:57 -0500

    Add portfolio value method

--- src/coin_test/analysis/tear_sheet.py
+++ src/coin_test/analysis/tear_sheet.py
@@ -1,6 +1,11 @@
 """Tear sheet class implementation."""
 
-from ..backtest import BacktestResults
+import pandas as pd
+
+
+from ..backtest import BacktestResults, Portfolio
+from ..data import Composer
+from ..util import AssetPair
 
 
 class TearSheet:
@@ -10,7 +15,21 @@ class TearSheet:
         """Initialize a TearSheet."""
         pass
 
-    # @staticmethod
-    # def from_backtest_results(backtest_results: BacktestResults) -> TearSheet:
-    #     """Make a TearSheet from backtest results."""
-    #     return TearSheet(backtest_results)
+    @staticmethod
+    def create_date_price_df(backtest_results: BacktestResults) -> pd.DataFrame:
+        """Create a TimeSeries dataframe for portfolio value over time."""
+        return pd.DataFrame()
+
+    @staticmethod
+    def value_from_portfolio(p: Portfolio, t: pd.Timestamp, c: Composer) -> float:
+        """Get the monetary value of a portfolio."""
+        base_currency = p.base_currency
+        all_prices = c.get_timestep(t)
+        total = p.assets[base_currency].qty
+
+        for ticker, money in p.assets.items():
+            asset_pair = AssetPair(base_currency, ticker)
+            conversion = all_prices[asset_pair]["Open"].iloc[0]
+            total += conversion * money.qty
+
+        return total
