9112e1b2f8afdec8619558b1f7c35017772ef221
Nathan Faber <ntfaber@gmail.com>
2023-02-20 11:14:00 -0500

    plot parameters

--- src/coin_test/analysis/data_processing.py
+++ src/coin_test/analysis/data_processing.py
@@ -1,6 +1,7 @@
 """Generate plots for analysis."""
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Sequence
 
 import datapane as dp
@@ -29,12 +30,56 @@ class DataframeGeneratorMultiple(ABC):
         """Create dataframe."""
 
 
+class PlotParameters:
+    """Plot parameters to pass to each plot."""
+
+    def __init__(
+        self,
+        line_styles: list[str] = ["solid", "dash", "dot", "dashdot"],
+        line_colors: list[str] = ["firebrick", "blue", "rebeccapurple"],
+        label_font_color: str = "black",
+        label_font_family: str = "Courier New, monospace",
+        title_font_size: int = 18,
+        axes_font_size: int = 10,
+        line_width: int = 2,
+    ) -> None:
+        self.line_styles = line_styles
+        self.line_colors = line_colors
+        self.line_width = line_width
+        self.title_font = dict(
+            family=label_font_family, size=title_font_size, color=label_font_color
+        )
+        self.axes_font = dict(
+            family=label_font_family, size=axes_font_size, color=label_font_color
+        )
+        self.legend_font = self.axes_font
+
+    @staticmethod
+    def update_plotly_fig(
+        plot_params: "PlotParameters",
+        fig: go.Figure,
+        title: str,
+        x_lbl: str,
+        y_lbl: str,
+        legend_title: str = "",
+    ) -> None:
+        """Update Plotly figure"""
+        fig.update_layout(
+            title={"text": title, "font": plot_params.title_font},
+            xaxis_title={"text": x_lbl, "font": plot_params.axes_font},
+            yaxis_title={"text": y_lbl, "font": plot_params.axes_font},
+            legend_title={"text": legend_title, "font": plot_params.legend_font},
+        )
+
+
 class SinglePlotGenerator(ABC):
     """Generate a plot using a single BacktestResults."""
 
     @staticmethod
     @abstractmethod
-    def create(backtest_results: BacktestResults) -> dp.Plot:
+    def create(
+        backtest_results: BacktestResults, plot_params: PlotParameters
+    ) -> dp.Plot:
         """Create plot object."""
 
 
@@ -43,25 +88,22 @@ class DistributionalPlotGenerator(ABC):
 
     @staticmethod
     @abstractmethod
-    def create(backtest_results_list: Sequence[BacktestResults]) -> dp.Plot:
+    def create(
+        backtest_results_list: Sequence[BacktestResults], plot_params: PlotParameters
+    ) -> dp.Plot:
         """Create distributional plot object."""
 
 
 class PricePlotSingle(SinglePlotGenerator):
     @staticmethod
-    def create(backtest_results: BacktestResults) -> dp.Plot:
+    def create(
+        backtest_results: BacktestResults, plot_params: PlotParameters
+    ) -> dp.Plot:
         """Create plot object."""
-
-        # df = px.data.gapminder().query("continent=='Oceania'")
-        # fig = px.line(backtest_results, x="Timestamp", y="lifeExp", color='country')
-
-        # fig = go.Figure([go.Scatter(x=backtest_results.sim_data['Timestamp'], y=backtest_results.sim_data['Price'])])
 
         df = pd.read_csv(
             "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv"
         )
-        #
-        # fig = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'], color_discrete_sequence="test" )])
         fig = px.line(
             x=df["Date"],
             y=df["AAPL.High"],
@@ -72,7 +114,7 @@ class PricePlotSingle(SinglePlotGenerator):
             xaxis_title="X Axis Title",
             yaxis_title="Y Axis Title",
             legend_title="Legend Title",
-            font=dict(family="Courier New, monospace", size=18, color="RebeccaPurple"),
+            font=plot_params.title_font,
         )
         return dp.Plot(fig)
 
@@ -81,20 +123,14 @@ class PricePlotMultiple(DistributionalPlotGenerator):
     """Create Price plot from multiple Datasets"""
 
     @staticmethod
-    def create(backtest_results: BacktestResults) -> dp.Plot:
+    def create(
+        backtest_results: BacktestResults, plot_params: PlotParameters
+    ) -> dp.Plot:
         """Create plot object."""
         df = pd.read_csv(
             "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv"
         )
 
-        # Look here for more data: https://plotly.com/python/line-charts/
-        fig = go.Figure(data=go.Scatter(x=df["Date"], y=df["AAPL.High"], name="main"))
-        fig.add_scatter(x=df["Date"], y=df["AAPL.Low"], mode="lines", name="Low")
-        fig.add_scatter(
-            x=df["Date"], y=df["AAPL.Open"], mode="lines+markers", name="Open"
-        )
-
-        # OR
         fig = go.Figure()
         # Create and style traces
         fig.add_trace(
@@ -102,7 +138,11 @@ class PricePlotMultiple(DistributionalPlotGenerator):
                 x=df["Date"],
                 y=df["AAPL.High"],
                 name="main",
-                line=dict(color="firebrick", width=4, dash="dashdot"),
+                line=dict(
+                    color=plot_params.line_colors[0],
+                    width=plot_params.line_width,
+                    dash=plot_params.line_styles[0],
+                ),
             )
         )
         fig.add_trace(
@@ -110,7 +150,11 @@ class PricePlotMultiple(DistributionalPlotGenerator):
                 x=df["Date"],
                 y=df["AAPL.Low"],
                 name="Low",
-                line=dict(width=4, dash="dot"),
+                line=dict(
+                    color=plot_params.line_colors[1],
+                    width=plot_params.line_width,
+                    dash=plot_params.line_styles[1],
+                ),
             )
         )
         fig.add_trace(
@@ -119,7 +163,9 @@ class PricePlotMultiple(DistributionalPlotGenerator):
                 y=df["AAPL.Open"],
                 name="Open",
                 line=dict(
-                    width=4, dash="dash"
+                    color=plot_params.line_colors[2],
+                    width=plot_params.line_width,
+                    dash=plot_params.line_styles[2],
                 ),  # dash options include 'dash', 'dot', and 'dashdot'
             )
         )
@@ -129,14 +175,7 @@ class PricePlotMultiple(DistributionalPlotGenerator):
         # fig.update_traces(hoverinfo='name', textfont_size=20,
         #           marker=dict(autocolorscale=False, line=dict(color=colors, width=2))) #color=colors
 
-        fig.update_layout(
-            title="Plot Title",
-            xaxis_title="X Axis Title",
-            yaxis_title="Y Axis Title",
-            legend_title="Legend Title",
-            font=dict(family="Courier New, monospace", size=18, color="RebeccaPurple"),
+        PlotParameters.update_plotly_fig(
+            plot_params, fig, "My title", "x_lbl", "y_lbl", "Legend"
         )
         return dp.Plot(fig)
-
-
-# columns=["Timestamp", "Portfolios", "Trades", "Pending Trades", "Price"],
--- src/coin_test/analysis/viz.py
+++ src/coin_test/analysis/viz.py
@@ -6,26 +6,18 @@ import datapane as dp
 import pandas as pd
 import plotly.express as px
 
-from coin_test.analysis.data_processing import PricePlotMultiple, PricePlotSingle
+from coin_test.analysis.data_processing import (
+    PlotParameters,
+    PricePlotMultiple,
+    PricePlotSingle,
+)
 
 if __name__ == "__main__":
 
-    df = px.data.gapminder()
+    plot_params = PlotParameters()
 
-    plotly_chart = px.scatter(
-        df.query("year==2007"),
-        x="gdpPercap",
-        y="lifeExp",
-        size="pop",
-        color="continent",
-        hover_name="country",
-        log_x=True,
-        size_max=60,
-    )
-    app = dp.App(dp.Plot(plotly_chart)).save(path="plotly-plot.html")
-
-    plot = PricePlotSingle.create(Mock())
-    multi_plot = PricePlotMultiple.create(Mock())
+    plot = PricePlotSingle.create(Mock(), plot_params)
+    multi_plot = PricePlotMultiple.create(Mock(), plot_params)
     app_nathan = dp.App(
         dp.Page(
             title="Coin-Test Datapane",
@@ -37,19 +29,3 @@ if __name__ == "__main__":
     )
 
     app_nathan.save(path="nathan-plot.html")
-
-    # app = dp.App(
-    #     dp.Page(
-    #         title="Titanic Dataset",
-    #         blocks=[
-    #             "### Dataset",
-    #             dp.Group(dp.Plot(plot), dp.DataTable(df), columns=2),
-    #         ],
-    #     ),
-    #     dp.Page(
-    #         title="Titanic Plot",
-    #         blocks=["### Plot", dp.Group(dp.Plot(plot), dp.DataTable(df), columns=2)],
-    #     ),
-    # )
-
-    # dp.Group(dp.Plot(plot), dp.DataTable(df), columns=2), dp.Group(dp.Plot(plot), dp.DataTable(df), columns=3))
